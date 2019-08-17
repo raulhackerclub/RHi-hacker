ff3e9959ef97ba485bd5af54a26397129889cf40
zecarhc <prototipados.io@gmail.com>
2019-08-17 12:17:46 -0300

    Raulifica sender e receiver

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,27 +1,44 @@
 /*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-*
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+ * RAUL HACKER CLUB 
+ * Projeto: RHi Hacker
+ * 
+ * Detalhe importante sobre o código:
+ * É necessário fazer o download da biblioteca NRF24: https://github.com/nRF24/RF24
+ * 
+ * Detalhes sobre conexões do hardware:
+ * Modelo de placa Arduino utilizada: MEGA 2560
+ * VCC: 1.9V ~ 3.6V
+ * CE: 7
+ * CSN: 8
+ * MOSI: 51 / 11[UNO]
+ * SCK: 52 / 13[UNO]
+ * MISO: 50 / 12[UNO]
+ * IRQ: <não será necessário usar este pino neste projeto>
 */
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
-void setup() {
+
+RF24 patrulha(7, 8); // CE, CSN
+
+const byte canal[6] = "77771";
+
+void setup() 
+{
   Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  patrulha.begin();
+  patrulha.openReadingPipe(0, canal);
+  patrulha.setPALevel(RF24_PA_MIN);
+  patrulha.startListening();
 }
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+
+void loop()
+ {
+  if( patrulha.available() ) 
+  {
+    char msg[32] = "";
+    patrulha.read(&msg, sizeof(msg));
+    Serial.println(msg);
   }
 }
